1186737380bdbdc4cd4194dd4c765375616f1634
Christian Tumyr <christian.tumyr@nav.no>
2023-11-14 17:17:16 +0100

    Legg til ArenaondemandtojoarkProperties

--- app/src/main/java/no/nav/arenaondemandtojoark/Application.java
+++ app/src/main/java/no/nav/arenaondemandtojoark/Application.java
@@ -1,9 +1,14 @@
 package no.nav.arenaondemandtojoark;
 
+import no.nav.arenaondemandtojoark.config.ArenaondemandtojoarkProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		ArenaondemandtojoarkProperties.class
+})
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
---
+++ app/src/main/java/no/nav/arenaondemandtojoark/config/ArenaondemandtojoarkProperties.java
@@ -0,0 +1,9 @@
+package no.nav.arenaondemandtojoark.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties("arenaondemandtojoark")
+public class ArenaondemandtojoarkProperties {
+}
