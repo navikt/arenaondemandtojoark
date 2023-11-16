f2320f7f5138586175750eb390d7b77ac9d3facd
Anders Opskar Voldsund <anders.voldsund@nav.no>
2023-11-16 14:19:18 +0100

    Fjern logging av Spring-banner og generated security password
    Co-authored-by: Christian Tumyr <christian.tumyr@nav.no>

--- app/src/main/java/no/nav/arenaondemandtojoark/Application.java
+++ app/src/main/java/no/nav/arenaondemandtojoark/Application.java
@@ -4,9 +4,10 @@ import no.nav.arenaondemandtojoark.config.ArenaondemandtojoarkProperties;
 import no.nav.arenaondemandtojoark.config.AzureTokenProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableConfigurationProperties({
 		ArenaondemandtojoarkProperties.class,
 		AzureTokenProperties.class
