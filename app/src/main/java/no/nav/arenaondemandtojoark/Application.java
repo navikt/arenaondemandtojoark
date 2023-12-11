c9c6e6e46a93884dbf27f2cacf92847b89bda50d
Anders Opskar Voldsund <anders.voldsund@nav.no>
2023-12-11 10:41:57 +0100

    Set web til NONE i SpringApplication sidan appen ikkje treng ein webserver
    Co-authored-by: Christian Tumyr <christian.tumyr@nav.no>

--- app/src/main/java/no/nav/arenaondemandtojoark/Application.java
+++ app/src/main/java/no/nav/arenaondemandtojoark/Application.java
@@ -2,10 +2,12 @@ package no.nav.arenaondemandtojoark;
 
 import no.nav.arenaondemandtojoark.config.ArenaondemandtojoarkProperties;
 import no.nav.arenaondemandtojoark.config.AzureTokenProperties;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import static org.springframework.boot.WebApplicationType.NONE;
 
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableConfigurationProperties({
@@ -14,6 +16,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 })
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		new SpringApplicationBuilder(Application.class)
+				.web(NONE)
+				.run(args);
 	}
 }
