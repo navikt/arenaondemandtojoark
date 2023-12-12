260f2e160bdfa04cb034a443fbd7aee39207ded2
Christian Tumyr <christian.tumyr@nav.no>
2023-12-12 14:52:38 +0100

    Legg til @Jacksonized for OpprettJournalpostRequest for samspill mellom Jackson og Lombok
    Co-authored-by: Anders Opskar Voldsund <anders.voldsund@nav.no>

--- app/src/main/java/no/nav/arenaondemandtojoark/consumer/dokarkiv/OpprettJournalpostRequest.java
+++ app/src/main/java/no/nav/arenaondemandtojoark/consumer/dokarkiv/OpprettJournalpostRequest.java
@@ -2,6 +2,7 @@ package no.nav.arenaondemandtojoark.consumer.dokarkiv;
 
 import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 import no.nav.arenaondemandtojoark.domain.joark.AvsenderMottaker;
 import no.nav.arenaondemandtojoark.domain.joark.Bruker;
 import no.nav.arenaondemandtojoark.domain.joark.Dokument;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Value
 @Builder
+@Jacksonized
 public class OpprettJournalpostRequest {
 	JournalpostType journalposttype;
 	AvsenderMottaker avsenderMottaker;
