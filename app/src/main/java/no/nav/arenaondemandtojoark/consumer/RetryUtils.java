237b275acb59325ce2635c797837dda1deb4ac31
Christian Tumyr <christian.tumyr@nav.no>
2023-12-18 14:28:27 +0100

    Fjern logging av retries

--- app/src/main/java/no/nav/arenaondemandtojoark/consumer/RetryUtils.java
+++ app/src/main/java/no/nav/arenaondemandtojoark/consumer/RetryUtils.java
@@ -1,6 +1,5 @@
 package no.nav.arenaondemandtojoark.consumer;
 
-import lombok.extern.slf4j.Slf4j;
 import no.nav.arenaondemandtojoark.exception.retryable.ArenaondemandtojoarkRetryableException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -8,7 +7,6 @@ import reactor.util.retry.Retry;
 
 import static java.time.Duration.ofMillis;
 
-@Slf4j
 @Component
 public class RetryUtils {
 
@@ -22,7 +20,6 @@ public class RetryUtils {
 	}
 
 	public static Retry backoffRetrySpec() {
-		log.info("Retryer {} ganger med {} ms mellom hver retry", retryMaxAttempts, retryTimeBetweenAttempts);
 		return Retry.backoff(retryMaxAttempts, ofMillis(retryTimeBetweenAttempts))
 				.filter(e -> e instanceof ArenaondemandtojoarkRetryableException)
 				.onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> retrySignal.failure());
