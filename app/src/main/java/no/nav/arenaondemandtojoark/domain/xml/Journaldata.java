66232172e0d855efb9937a0830c6e5e9c78d1ba9
Christian Tumyr <christian.tumyr@nav.no>
2024-03-05 10:46:07 +0100

    Deaktiver mapping fagområde KLA til AAP under innlesing

--- app/src/main/java/no/nav/arenaondemandtojoark/domain/xml/Journaldata.java
+++ app/src/main/java/no/nav/arenaondemandtojoark/domain/xml/Journaldata.java
@@ -2,10 +2,8 @@ package no.nav.arenaondemandtojoark.domain.xml;
 
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import lombok.Getter;
 import lombok.Setter;
-import no.nav.arenaondemandtojoark.domain.xml.adapters.FagomraadeAdapter;
 
 @Getter
 @Setter
@@ -16,7 +14,7 @@ public class Journaldata {
 	String brukerId;
 	String brukerType;
 	String journalpostType;
-	@XmlJavaTypeAdapter(FagomraadeAdapter.class)
+	//@XmlJavaTypeAdapter(FagomraadeAdapter.class)
 	String fagomraade;
 	String journaldato;
 	String innhold;
