819f17e1f14d09a3fbbf619544c9afad43fe455e
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2024-06-14 08:39:01 +0200

    Oppsett for å bruke ny jdbc_url

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env sh
 
-if test -f /var/run/secrets/nais.io/arenaondemandtojoarkDB/username;
+if test -f /var/run/secrets/nais.io/db_creds/username;
 then
     echo "Setting SPRING_DATASOURCE_USERNAME"
-    export SPRING_DATASOURCE_USERNAME=$(cat /var/run/secrets/nais.io/arenaondemandtojoarkDB/username)
+    export SPRING_DATASOURCE_USERNAME=$(cat /var/run/secrets/nais.io/db_creds/username)
 fi
 
-if test -f /var/run/secrets/nais.io/arenaondemandtojoarkDB/password;
+if test -f /var/run/secrets/nais.io/db_creds/password;
 then
     echo "Setting SPRING_DATASOURCE_PASSWORD"
-    export SPRING_DATASOURCE_PASSWORD=$(cat /var/run/secrets/nais.io/arenaondemandtojoarkDB/password)
+    export SPRING_DATASOURCE_PASSWORD=$(cat /var/run/secrets/nais.io/db_creds/password)
+fi
+
+if test -f /var/run/secrets/nais.io/db_config/jdbc_url;
+then
+    export SPRING_DATASOURCE_URL=$(cat /var/run/secrets/nais.io/db_config/jdbc_url)
+    echo "Setting SPRING_DATASOURCE_URL=$SPRING_DATASOURCE_URL"
 fi
 
 # sftp
